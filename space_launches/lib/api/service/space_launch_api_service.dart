d24d615378d47ea128ceb96816f1f3852fd2de5e
Anna Domashych <anna.domashych@gmail.com>
2021-03-28 14:45:27 +0300

    10. Implement SpaceLaunchApiService

---
+++ space_launches/lib/api/service/space_launch_api_service.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:space_launches/api/models/space_launch.dart';
+
+part 'space_launch_api_service.g.dart';
+
+@RestApi(baseUrl: "https://ll.thespacedevs.com/2.0.0/")
+abstract class SpaceLaunchApiService {
+  factory SpaceLaunchApiService(Dio dio, {String? baseUrl}) =
+      _SpaceLaunchApiService;
+
+  @GET('/launch/upcoming')
+  Future<UpcomingLaunches> getUpcomingLaunches();
+
+  @GET('/launch/{id}')
+  Future<SpaceLaunch> getUpcomingLaunch(@Path("id") String id);
+}
