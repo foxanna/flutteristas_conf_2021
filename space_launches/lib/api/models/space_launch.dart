bfc5554c3507a4545003f75a8e338eb817e70c5a
Anna Domashych <anna.domashych@gmail.com>
2021-03-28 14:36:37 +0300

    06. Implement SpaceLaunch.fromJson

--- space_launches/lib/api/models/space_launch.dart
+++ space_launches/lib/api/models/space_launch.dart
@@ -1,6 +1,7 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'space_launch.freezed.dart';
+part 'space_launch.g.dart';
 
 @freezed
 class SpaceLaunch with _$SpaceLaunch {
@@ -8,11 +9,14 @@ class SpaceLaunch with _$SpaceLaunch {
     required String id,
     required String name,
     required String image,
-    required DateTime windowStart,
-    required DateTime windowEnd,
+    @JsonKey(name: 'window_start') required DateTime windowStart,
+    @JsonKey(name: 'window_end') required DateTime windowEnd,
     LaunchStatus? status,
     LaunchMission? mission,
   }) = _SpaceLaunch;
+
+  factory SpaceLaunch.fromJson(Map<String, dynamic> json) =>
+      _$SpaceLaunchFromJson(json);
 }
 
 @freezed
@@ -20,6 +24,9 @@ class LaunchStatus with _$LaunchStatus {
   const factory LaunchStatus({
     required String name,
   }) = _LaunchStatus;
+
+  factory LaunchStatus.fromJson(Map<String, dynamic> json) =>
+      _$LaunchStatusFromJson(json);
 }
 
 @freezed
@@ -28,4 +35,7 @@ class LaunchMission with _$LaunchMission {
     required String name,
     required String description,
   }) = _LaunchMission;
+
+  factory LaunchMission.fromJson(Map<String, dynamic> json) =>
+      _$LaunchMissionFromJson(json);
 }
